883a47ab24b20652ed0a799e98bf3826c586eab5
k-ra <kyraymo@gmail.com>
2024-07-09 16:09:17 -0400

    Initial Commit

---
+++ thinkingcap/thinkingcapApp.swift
@@ -0,0 +1,17 @@
+//
+//  thinkingcapApp.swift
+//  thinkingcap
+//
+//  Created by kyra on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct thinkingcapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
